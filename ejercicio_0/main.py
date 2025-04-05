4d9c9f216975a15359fe5b6ed7f8e95ab165d443
Matiasjs1 <162461241+Matiasjs1@users.noreply.github.com>
2025-04-05 15:22:04 -0300

    Delete ejercicio_0 directory

--- ejercicio_0/main.py
+++
@@ -1,9 +0,0 @@
-from automovil import Automovil
-import os
-
-if __name__ == "__main__":
-    auto1 = Automovil("Toyota", "Corolla", 2023, "A58S6","Matias",47432404)
-    auto1.mostra_info()
-    auto1.mostrar_info_duenio()
-    print(f"Patente: {auto1.get_patente()}")
-    auto1.arrancar()
